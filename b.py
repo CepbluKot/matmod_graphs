8a1c1ac49a7f2d31912c15760b86abe9a78e3c12
CeplbuKot <igorcat100@gmail.com>
2021-11-26 23:17:15 +0300

    all f

--- b.py
+++ b.py
@@ -15,10 +15,68 @@ def nu2(x, a, b):
     return res
 
 
-def fx(t):
+def nu3(x, a, b):
 
-    res = 1000 * math.sqrt(2) / 2 * t
+    res = 2 * ((x-a)/(b-a))**2
     return res
+
+
+def nu4(x, a, b):
+
+    res = 1 - 2 *((x - b) / (b-a))**2
+    return res
+
+
+def zFunc(a, b):
+
+    x = a
+
+    while x < b:
+
+        funcX.append(x)
+
+        if a < x and x <= (a+b)/2:
+
+            funcY.append(nu1(x, a, b))
+
+        elif (a+b)/2 < x and x < b:
+            funcY.append(nu2(x, a, b))
+
+        else:
+            funcY.append(1)
+
+        x += 0.5
+
+
+def sFunc(a, b):
+
+    x = a
+
+    while x < b:
+        y = (a + b) / 2
+        funcX.append(x)
+
+        if x <= a:
+            funcY.append(0)
+
+        elif a < x and x <= y:
+
+            funcY.append(nu3(x, a, b))
+
+        elif y < x and x < b:
+            funcY.append(nu4(x, a, b))
+
+        x += 0.5
+
+
+
+
+def piFunc(a, b):
+
+    sFunc(a, b)
+    zFunc(b, 2 * (b - a))
+
+    
 
 
 funcX = []
@@ -30,24 +88,8 @@ h = 5
 x = 0
 a = 0
 b = 20
-while x < b:
+piFunc(a, b)
 
-    funcX.append(x)
-
-    if a < x and x <= (a+b)/2:
-        
-        funcY.append(nu1(x, a, b))
-
-    elif (a+b)/2 < x and x < b:
-        funcY.append(nu2(x, a, b))
-
-    else:
-        funcY.append(1)
-    
-    x += 0.5
-
-print('x:', funcX)
-print('y:', funcY)
 
 fig = plt.figure()
 
