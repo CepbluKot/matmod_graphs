57aa7083f28706eaefc664f639e6e1a2387cb5a7
CepbluKot <igorcat100@gmail.com>
2021-11-27 12:54:08 +0300

    smg

--- b.py
+++ b.py
@@ -28,26 +28,45 @@ def nu4(x, a, b):
     return res
 
 
-def zFunc(a, b, c='r', axNum=1, temp=False, t=0, label=''):
+def zFunc(a, b, c='r', axNum=1, temp=False, t=0, label='', intP = 0):
     funcX = []
     funcY = []
     x = a
 
+    i = intP
+    j = 0
+    fx = 0
+    fy = 0
     while x < b:
 
         funcX.append(x)
 
         if a < x and x <= (a+b)/2:
-
-            funcY.append(nu1(x, a, b))
+            
+            j = nu1(x, a, b)
+            if x >  i - 0.1 and x < i + 0.1 and intP != 0:
+                fx = x
+                fy = j
+            funcY.append(j)
 
         elif (a+b)/2 < x and x < b:
-            funcY.append(nu2(x, a, b))
+
+            j = nu2(x, a, b)
+            
+            if x >  i - 0.1 and x < i + 0.1 and intP != 0:
+                fx = x
+                fy = j
+                
+                
+            funcY.append(j)
 
         else:
             funcY.append(1)
-
+        
         x += 0.01
+    print(label,fx, fy)
+
+        
     if axNum == 1:
         ax1.plot(funcX, funcY, c)
         if temp == True:
@@ -79,11 +98,14 @@ def zFunc(a, b, c='r', axNum=1, temp=False, t=0, label=''):
     
 
 
-def sFunc(a, b, c='b', axNum=1, temp=False, t=0, label=''):
+def sFunc(a, b, c='b', axNum=1, temp=False, t=0, label='', intP=0):
     funcX = []
     funcY = []
     x = a
-
+    i = intP
+    fx= 0
+    fy=0
+    j = 0
     while x < b:
         y = (a + b) / 2
         funcX.append(x)
@@ -92,13 +114,22 @@ def sFunc(a, b, c='b', axNum=1, temp=False, t=0, label=''):
             funcY.append(0)
 
         elif a < x and x <= y:
+            j = nu3(x, a, b)
 
-            funcY.append(nu3(x, a, b))
+            if x >  i - 0.1 and x < i + 0.1 and intP != 0:
+                fx = x
+                fy = j
+            funcY.append(j)
 
         elif y < x and x < b:
-            funcY.append(nu4(x, a, b))
+            j = nu4(x, a, b)
+            if x >  i - 0.1 and x < i + 0.1 and intP != 0:
+                fx = x
+                fy = j
+            funcY.append(j)
 
         x += 0.01
+    print(label,fx, fy)
 
     if axNum == 1:
         ax1.plot(funcX, funcY, c)
@@ -161,24 +192,24 @@ temp = 35
 pechT = 700
 speed = 1
 
-zFunc(1, 50, 'r', 1, True, temp, 'temp')
-sFunc(15, 55, 'y', 1, True, temp)
+zFunc(1, 50, 'r', 1, True, temp, 'temp', 35)
+sFunc(15, 55, 'y', 1, True, temp, '', 35)
 zFunc(25 + 30, 95, 'y', 1, False, 0)
 sFunc(50, 100, 'b', 1, False, 0)
 
-zFunc(100, 700, 'r', 2, True, pechT, 'Tpech')
+zFunc(100, 700, 'r', 2, True, pechT, 'Tpech', pechT)
 sFunc(700 - ((850 + 701)/2 - 700), 700, 'y', 2, True, pechT)
 lineFunc(700, 850, 'y', 2, True, pechT)
 zFunc(850, 950, 'y', 2, True, pechT)
 sFunc(851, 950, 'b', 2, True, pechT)
 
-zFunc(0, 1.001, 'b', 3, True, speed,'speed')
+zFunc(0, 1.001, 'b', 3, True, speed,'speed', speed)
 sFunc(0, 1.001, 'r', 3, False, 0)
 
 
 #  1 2 3
 
-zFunc(0, 1.5, 'r', 4)
+zFunc(0, 1.5, 'r', 4, False, 0, 'Mode')
 sFunc(0, 1.5, 'y', 4)
 zFunc(1.5, 3, 'y', 4)
 sFunc(1.5, 3, 'b', 4)
